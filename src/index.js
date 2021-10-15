d5e5aa4ee8242d3064ce127925c2cbf2f97758bc
abdulwahid284 <abdul942@gmail.com>
2021-10-15 11:18:58 +0530

    1st commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './front-bone/css/index.css';
+// import App from '../src/front-bone/js/App';
+
+ReactDOM.render(
+  <h1 className="">Hello World!</h1>,
+  document.getElementById('root')
+);
